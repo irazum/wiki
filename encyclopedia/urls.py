cb31d73a158b561c7c7e809297ab900a78fca9b6
Irazum <conscientiaaa@gmail.com>
2020-11-24 14:58:06 +0300

    v0.09

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     re_path(r"EditPage/(?P<title>[\w\s]+)/", views.edit_page, name='EditPage'),
     path("NewPage/", views.new_page, name="NewPage"),
     path("", views.index, name="index"),
-    path(r"^search/$", views.send_search, name="search"),
+    path(r"search/", views.send_search, name="search"),
     re_path(r"(?P<title>[\w\s]+)/", views.send_entry, name="entry")
 
 ]
